4324ea5db2256eb01d983c5d44c29e60d8de4eff
Lucia Ristea <luciaristea@Lucias-MacBook-Pro.local>
2020-04-10 10:28:22 -0400

    input menu now works

--- hogwartsStudents.c
+++ hogwartsStudents.c
@@ -25,7 +25,7 @@ Student* delete(Student** root, char* first, char* last)
 
 }
 
-int main(int argc, char **argv)
+int main()
 {
   Student student;
 
@@ -36,10 +36,7 @@ int main(int argc, char **argv)
   Student* deceasedRoot = NULL;
   Student* found = NULL;
 
-  char input[20];
-  scanf("%s\n", input);
-
-  int i = 1;
+  //int i = 1;
   char* filename;
   char* firstname;
   char* lastname;
@@ -48,105 +45,99 @@ int main(int argc, char **argv)
   int number;
   House house;
 
-  while(i < argc)
+  char input[20];
+
+   while(strcmp(input, "quit") != 0)
   {
-    if (strcmp(input, "quit") != 0)
-    {
-       printf("Please enter a command.\n");
+    printf("Please enter a command.\n");
+    scanf("%[^\n]%*c", input);
 
-       if (strcmp(input, "help") == 0)
-       {
-         printf("load: \nAdds all the student records in <filename> to the roster. The file is arranged one student per line where each record has the following format.");
-         printf("save: \nSave the records of all living students in the roster to <filename>. ");
-         printf("clear: \nClear the rosters for the four houses and for the list of deceased students. Delete all allocated memory.");
-         printf("inorder: \nPrint out each house in an inorder traversal.");
-         printf("preorder: \nPrint out each house in a preorder traversal.");
-         printf("postorder: \nPrint out each house in a postorder traversal.");
-         printf("add: \nAdd a student with the given attributes to the roster.");
-         printf("kill: \nFind the given student in the given house's roster, remove the student from the house's roster, and add the student to the list of deceased students.");
-         printf("find: \nFind the given student in the given house's roster and print out his or her data.");
-         printf("points: \nFind the given student in the given house's roster and add <number> points to their point total.");
-         printf("score: \nPrint out the current point scores for all houses. Points for deceased students are not factored into their house totals");
-         printf("quit: \nQuit the program. ");
-       }
-       if (strcmp(input, "load") == 0)
-       {
-         ++i;
-        filename = argv[i];
-       }
-       if (strcmp(input, "save") == 0)
-       {
-         ++i;
-        filename = argv[i];
-       }
-       if (strcmp(input, "clear") == 0)
-       {
-
-       }
-       if (strcmp(input, "inorder") == 0)
-       {
-
-       }
-       if (strcmp(input, "preorder") == 0)
-       {
-
-       }
-       if (strcmp(input, "postorder") == 0)
-       {
-
-       }
-       if (strcmp(input, "add") == 0)
-       {
-         ++i;
-         firstname = argv[i];
-         ++i;
-         lastname = argv[i];
-         ++i;
-         points = argv[i];
-         ++i;
-         year = argv[i];
-         ++i;
-         house = argv[i];
-       }
-       if (strcmp(input, "kill") == 0)
-       {
-        ++i;
-        firstname = argv[i];
-        ++i;
-        lastname = argv[i];
-        ++i;
-        house = argv[i];
-       }
-       if (strcmp(input, "find") == 0)
-       {
-         ++i;
-         firstname = argv[i];
-         ++i;
-         lastname = argv[i];
-         ++i;
-         house = argv[i];
-       }
-       if (strcmp(input, "points") == 0)
-       {
-         ++i;
-         firstname = argv[i];
-         ++i;
-         lastname = argv[i];
-         ++i;
-         house = argv[i];
-         ++i;
-         number = argv[i];
-       }
-       if (strcmp(input, "score") == 0)
-       {
-
-       }
-
-    //quit();
-
-    }
+     if (strcmp(input, "help") == 0)
+     {
+       printf("load: \nAdds all the student records in <filename> to the roster. The file is arranged one student per line where each record has the following format.\n");
+       printf("\n");
+       printf("save: \nSave the records of all living students in the roster to <filename>.\n");
+       printf("\n");
+       printf("clear: \nClear the rosters for the four houses and for the list of deceased students. Delete all allocated memory.\n");
+       printf("\n");
+       printf("inorder: \nPrint out each house in an inorder traversal.\n");
+       printf("\n");
+       printf("preorder: \nPrint out each house in a preorder traversal.\n");
+       printf("\n");
+       printf("postorder: \nPrint out each house in a postorder traversal.\n");
+       printf("\n");
+       printf("add: \nAdd a student with the given attributes to the roster.\n");
+       printf("\n");
+       printf("kill: \nFind the given student in the given house's roster, remove the student from the house's roster, and add the student to the list of deceased students.\n");
+       printf("\n");
+       printf("find: \nFind the given student in the given house's roster and print out his or her data.\n");
+       printf("\n");
+       printf("points: \nFind the given student in the given house's roster and add <number> points to their point total.\n");
+       printf("\n");
+       printf("score: \nPrint out the current point scores for all houses. Points for deceased students are not factored into their house totals\n");
+       printf("\n");
+       printf("quit: \nQuit the program.\n");
+       printf("\n");
+     }
+     if (strcmp(input, "load") == 0)
+     {
+      filename = scanf("%[^\n]%*c", filename);
+     }
+     if (strcmp(input, "save") == 0)
+     {
+      filename = scanf("%[^\n]%*c", filename);
+     }
+     if (strcmp(input, "clear") == 0)
+     {
+       printf("call clear().\n");
+     }
+     if (strcmp(input, "inorder") == 0)
+     {
+       printf("call inorder().\n");
+     }
+     if (strcmp(input, "preorder") == 0)
+     {
+       printf("call preorder().\n");
+     }
+     if (strcmp(input, "postorder") == 0)
+     {
+       printf("call postorder().\n");
+     }
+     if (strcmp(input, "add") == 0)
+     {
+       firstname = scanf("%[^\n]%*c", firstname);
+       lastname = scanf("%[^\n]%*c", lastname);
+       points = scanf("%d", &points);
+       year = scanf("%d", &year);
+       house = scanf("%[^\n]%*c", house);
+     }
+     if (strcmp(input, "kill") == 0)
+     {
+      firstname = scanf("%[^\n]%*c", firstname);
+      lastname = scanf("%[^\n]%*c", lastname);
+      house = scanf("%[^\n]%*c", house);
+     }
+     if (strcmp(input, "find") == 0)
+     {
+       firstname = scanf("%[^\n]%*c", firstname);
+       lastname = scanf("%[^\n]%*c", lastname);
+       house = scanf("%[^\n]%*c", house);
+     }
+     if (strcmp(input, "points") == 0)
+     {
+       firstname = scanf("%[^\n]%*c", firstname);
+       lastname = scanf("%[^\n]%*c", lastname);
+       house = scanf("%[^\n]%*c", house);
+       number = scanf("%d", &number);
+     }
+     if (strcmp(input, "score") == 0)
+     {
+       printf("call score()\n");
+     }
 
   }
+
+  printf("call quit()\n");
 
   return 0;
 }
